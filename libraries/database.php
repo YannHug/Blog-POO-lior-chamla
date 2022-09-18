b176e1e75158ae03ae1db4890abce4dd9eca3ed4
Yannh <yannhweb@gmail.com>
2022-09-18 18:57:02 +0200

    singleton

--- libraries/database.php
+++ libraries/database.php
@@ -2,6 +2,9 @@
 
 class Database
 {
+
+    private static $instance = null;
+
     /**
      * Retourne une connexion à la base de données
      *
@@ -14,10 +17,14 @@ class Database
          * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
          * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
          */
-        $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', 'root', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
-        return $pdo;
+
+        if (self::$instance === null) {
+            self::$instance = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', 'root', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
+
+        return self::$instance;
     }
 }
